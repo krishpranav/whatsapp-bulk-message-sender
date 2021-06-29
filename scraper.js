123caee684d3010af13ea5fca385bed86a370a7c
krishpranav <krisna.pranav@gmail.com>
2021-06-29 19:19:28 +0530

    [+] variables [+]

---
+++ scraper.js
@@ -0,0 +1,17 @@
+erfan4lx = (function(){
+    
+    MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+    var SCROLL_INTERVAL = 600, 
+        SCROLL_INCREMENT = 450, 
+        AUTO_SCROLL = true,
+        NAME_PREFIX = '',
+        UNKNOWN_CONTACTS_ONLY = false, 
+        MEMBERS_QUEUE = {},
+        TOTAL_MEMBERS;
+
+    var scrollInterval, observer, membersList, header;
+
+})();
+
+erfan4lx.quickExport()
