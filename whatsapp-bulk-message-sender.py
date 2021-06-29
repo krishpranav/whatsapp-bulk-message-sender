d79fab1ff1297355ac6255ca64836f5694d219e0
krishpranav <krisna.pranav@gmail.com>
2021-06-29 19:27:37 +0530

    [+] open function [+]

--- whatsapp-bulk-message-sender.py
+++ whatsapp-bulk-message-sender.py
@@ -9,3 +9,10 @@ messages = input(str("What's the message you want to send => "))
 files = input(str("File of contacts => "))
 
 users = []
+with open(r"{}".format(files), encoding='UTF-8') as f:
+    rows = csv.reader(f,delimiter=",",lineterminator="\n")
+    next(rows, None)
+    for row in rows:
+        user = row[0]
+        users.append(user)
+        
